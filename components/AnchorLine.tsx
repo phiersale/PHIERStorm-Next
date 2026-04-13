c85ffc2da654e9de8f13ac24e48f01e58f31e252
phiersale <will@phiers.org>
2026-04-13 01:25:56 -0700

    Create AnchorLine.tsx

---
+++ components/AnchorLine.tsx
@@ -0,0 +1,8 @@
+export default function AnchorLine() {
+  return (
+    <div className="anchor-line">
+      <p>Nothing changes until ignoring people<br />costs more than responding to them.</p>
+      <span className="highlight">PHIERS is how we raise that cost.</span>
+    </div>
+  )
+}
